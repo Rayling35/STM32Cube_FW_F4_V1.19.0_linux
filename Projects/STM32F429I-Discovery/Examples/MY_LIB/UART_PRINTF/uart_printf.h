d212a43d8a82692498b22c129ba12d42efc5016f
Rayling <honwe.lin@gmail.com>
2018-02-09 17:45:27 +0800

    Define change of USARTxPrintf_FORCE_RESET() and USARTxPrintf_RELEASE_RESET()

--- Projects/STM32F429I-Discovery/Examples/MY_LIB/UART_PRINTF/uart_printf.h
+++ Projects/STM32F429I-Discovery/Examples/MY_LIB/UART_PRINTF/uart_printf.h
@@ -10,8 +10,8 @@
 #define USARTxPrintf_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE()
 #define USARTxPrintf_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOA_CLK_ENABLE() 
 
-#define USARTxPrintf_FORCE_RESET()             __HAL_RCC_USART2_FORCE_RESET()
-#define USARTxPrintf_RELEASE_RESET()           __HAL_RCC_USART2_RELEASE_RESET()
+#define USARTxPrintf_FORCE_RESET()             __HAL_RCC_USART1_FORCE_RESET()
+#define USARTxPrintf_RELEASE_RESET()           __HAL_RCC_USART1_RELEASE_RESET()
 
 /* Definition for USARTxPrintf Pins */
 #define USARTxPrintf_TX_PIN                    GPIO_PIN_9
