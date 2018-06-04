540a2768e53e05448312881aee17be4abbe99968
Rayling <honwe.lin@gmail.com>
2018-06-04 13:34:19 +0800

    [Update] arch_driver_ht1621 of main.c update

--- Projects/STM32F429I-Discovery/Examples/MYPJ/arch_driver_ht1621/Src/main.c
+++ Projects/STM32F429I-Discovery/Examples/MYPJ/arch_driver_ht1621/Src/main.c
@@ -6,29 +6,6 @@
 #include "main.h"
 
 
-void seg7(struct device *dev, uint16_t select, uint8_t a, uint8_t b, uint8_t c,
-	        uint8_t d, uint8_t e, uint8_t f, uint8_t g, uint16_t function)
-{
-	if(select == 0 || select == 2 || select == 4 || select == 6 || select == 8 ||
-		select == 10 || select == 12 || select == 14) {
-			
-			seg7_write_data_bit_4(dev, select+0, a, f, e, d);
-			seg7_write_data_bit_4(dev, select+1, b, g, c, function);
-			
-	}else if(select == 16 || select == 18 || select == 20 || select == 22 || select == 24 ||
-		select == 26 || select == 28 || select == 30) {
-			
-			seg7_write_data_bit_4(dev, select+0, d, e, f, a);
-			seg7_write_data_bit_4(dev, select+1, function, c, g, b);
-	}
-}
-
-/* number[7]: abcdefg */
-void seg7_show(struct device *dev, uint16_t select, uint8_t *number, uint16_t function)
-{
-	seg7(dev,select,number[0],number[1],number[2],number[3],number[4],number[5],number[6],function);
-}
-
 void seg7_set_all(struct device *dev)
 {
 	uint16_t i;
@@ -51,52 +28,62 @@ int main(void)
 	uart_printf_init();
 	
 	struct device *ht1621 = ht1621_device_binding();
+	
 	seg7_init(ht1621);
+	
 	seg7_write_command(ht1621, BIAS);
 	seg7_write_command(ht1621, RC256);
 	seg7_write_command(ht1621, SYSEN);
 	seg7_write_command(ht1621, LCDON);
 	
-	printf("LCD device init\r\n");
-	
+	printf("All device init\r\n");
 	
 	seg7_set_all(ht1621);
 	HAL_Delay(500);
 	seg7_clean_all(ht1621);
 	HAL_Delay(500);
 	
-	//abcdefg
-	uint8_t no0[7] = {1,1,1,1,1,1,0}; //0
-	uint8_t no1[7] = {0,1,1,0,0,0,0}; //1
-	uint8_t no2[7] = {1,1,0,1,1,0,1}; //2
-	uint8_t no3[7] = {1,1,1,1,0,0,1}; //3
-	uint8_t no4[7] = {0,1,1,0,0,1,1}; //4
-	uint8_t no5[7] = {1,0,1,1,0,1,1}; //5
-	uint8_t no6[7] = {1,0,1,1,1,1,1}; //6
-	uint8_t no7[7] = {1,1,1,0,0,1,0}; //7
-	uint8_t no8[7] = {1,1,1,1,1,1,1}; //8
-	uint8_t no9[7] = {1,1,1,1,0,1,1}; //9
+	uint16_t i;
 	
 	while(1) {
-		seg7_show(ht1621, 0, no0, 0);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no1, 1);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no2, 0);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no3, 1);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no4, 0);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no5, 1);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no6, 0);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no7, 1);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no8, 0);
-		HAL_Delay(100);
-		seg7_show(ht1621, 0, no9, 1);
-		HAL_Delay(100);
+		for(i = 0; i < 32; i++) {
+			seg7_write_data_4(ht1621, i, 0x01); //COM:0001
+			HAL_Delay(100);
+		}
+		
+		for(i = 0; i < 32; i++) {
+			seg7_write_data_4(ht1621, i, 0x03); //COM:0011
+			HAL_Delay(100);
+		}
+		
+		for(i = 0; i < 32; i++) {
+			seg7_write_data_4(ht1621, i, 0x07); //COM:0111
+			HAL_Delay(100);
+		}
+		
+		for(i = 0; i < 32; i++) {
+			seg7_write_data_4(ht1621, i, 0x0F); //COM:1111
+			HAL_Delay(100);
+		}
+		
+		for(i = 0; i < 32; i++) {
+			seg7_write_data_bit_4(ht1621, i, 1, 1, 1, 0); //COM:1110
+			HAL_Delay(100);
+		}
+		
+		for(i = 0; i < 32; i++) {
+			seg7_write_data_bit_4(ht1621, i, 1, 1, 0, 0); //COM:1100
+			HAL_Delay(100);
+		}
+		
+		for(i = 0; i < 32; i++) {
+			seg7_write_data_bit_4(ht1621, i, 1, 0, 0, 0); //COM:1000
+			HAL_Delay(100);
+		}
+		
+		for(i = 0; i < 32; i++) {
+			seg7_write_data_bit_4(ht1621, i, 0, 0, 0, 0); //COM:0000
+			HAL_Delay(100);
+		}
 	}
 }
