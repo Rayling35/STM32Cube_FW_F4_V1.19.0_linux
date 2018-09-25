91c1c7d40d075b1d78218e9ba4b99272b5b404c7
Rayling <honwe.lin@gmail.com>
2018-09-26 00:06:27 +0800

    [Update] Project of arch_driver_ht1621 some define modify

--- Projects/STM32F429I-Discovery/Examples/MYPJ/arch_driver_ht1621/Src/main.c
+++ Projects/STM32F429I-Discovery/Examples/MYPJ/arch_driver_ht1621/Src/main.c
@@ -6,23 +6,23 @@
 #include "stm32f4xx_hal.h"
 #include "device.h"
 #include "ht1621_driver.h"
-#include "seg7_common_api.h"
+#include "api_seg7_common.h"
 #include "main.h"
 
 
-void seg7_set_all(struct device *dev)
+void seg7_set_all(struct device *Dev)
 {
 	uint16_t i;
 	for(i = 0; i < 0x3F; i++) {        //A5~A0: 00111111
-		seg7_write_data_4(dev, i, 0x0F); //D3~D0: 00001111 set 1
+		seg7_write_data_4(Dev, i, 0x0F); //D3~D0: 00001111 set 1
 	}
 }
 
-void seg7_clean_all(struct device *dev)
+void seg7_clean_all(struct device *Dev)
 {
 	uint16_t i;
 	for(i = 0; i < 0x3F; i++) {        //A5~A0: 00111111
-		seg7_write_data_4(dev, i, 0x00); //D3~D0: 00001111 set 0
+		seg7_write_data_4(Dev, i, 0x00); //D3~D0: 00001111 set 0
 	}
 }
 
@@ -31,62 +31,62 @@ int main(void)
 	system_initialization();
 	uart_printf_init();
 	
-	struct device *ht1621 = ht1621_device_binding();
+	struct device *Ht1621 = ht1621_device_binding();
 	
-	seg7_init(ht1621);
+	seg7_init(Ht1621);
 	
-	seg7_write_command(ht1621, BIAS);
-	seg7_write_command(ht1621, RC256);
-	seg7_write_command(ht1621, SYSEN);
-	seg7_write_command(ht1621, LCDON);
+	seg7_write_command(Ht1621, BIAS);
+	seg7_write_command(Ht1621, RC256);
+	seg7_write_command(Ht1621, SYSEN);
+	seg7_write_command(Ht1621, LCDON);
 	
 	printf("All device init\r\n");
 	
-	seg7_set_all(ht1621);
+	seg7_set_all(Ht1621);
 	HAL_Delay(500);
-	seg7_clean_all(ht1621);
+	seg7_clean_all(Ht1621);
 	HAL_Delay(500);
 	
 	uint16_t i;
 	
 	while(1) {
 		for(i = 0; i < 32; i++) {
-			seg7_write_data_4(ht1621, i, 0x01); //COM:0001
+			seg7_write_data_4(Ht1621, i, 0x01); //COM:0001
 			HAL_Delay(100);
 		}
 		
 		for(i = 0; i < 32; i++) {
-			seg7_write_data_4(ht1621, i, 0x03); //COM:0011
+			seg7_write_data_4(Ht1621, i, 0x03); //COM:0011
 			HAL_Delay(100);
 		}
 		
 		for(i = 0; i < 32; i++) {
-			seg7_write_data_4(ht1621, i, 0x07); //COM:0111
+			seg7_write_data_4(Ht1621, i, 0x07); //COM:0111
 			HAL_Delay(100);
 		}
 		
 		for(i = 0; i < 32; i++) {
-			seg7_write_data_4(ht1621, i, 0x0F); //COM:1111
+			seg7_write_data_4(Ht1621, i, 0x0F); //COM:1111
 			HAL_Delay(100);
 		}
 		
 		for(i = 0; i < 32; i++) {
-			seg7_write_data_bit_4(ht1621, i, 1, 1, 1, 0); //COM:1110
+			seg7_write_data_bit_4(Ht1621, i, 1, 1, 1, 0); //COM:1110
 			HAL_Delay(100);
 		}
 		
 		for(i = 0; i < 32; i++) {
-			seg7_write_data_bit_4(ht1621, i, 1, 1, 0, 0); //COM:1100
+			seg7_write_data_bit_4(Ht1621, i, 1, 1, 0, 0); //COM:1100
 			HAL_Delay(100);
 		}
 		
 		for(i = 0; i < 32; i++) {
-			seg7_write_data_bit_4(ht1621, i, 1, 0, 0, 0); //COM:1000
+			seg7_write_data_bit_4(Ht1621, i, 1, 0, 0, 0); //COM:1000
 			HAL_Delay(100);
 		}
 		
 		for(i = 0; i < 32; i++) {
-			seg7_write_data_bit_4(ht1621, i, 0, 0, 0, 0); //COM:0000
+			seg7_write_data_bit_4(Ht1621, i, 0, 0, 0, 0); //COM:0000
 			HAL_Delay(100);
 		}
 	}
